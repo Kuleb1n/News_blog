32038dca1c175d47139f838b56cbc992b57cf033
Kuleb1n <v_vpyt@mail.ru>
2022-11-14 22:03:48 +0300

    registration of the model in the admin panel

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import News, Category
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'created_at', 'updated_at', 'photo', 'is_published', 'category']
+    list_display_links = ['id', 'title', 'category']
+    list_filter = ['title', 'created_at', 'updated_at', 'is_published', 'category']
+    search_fields = ['title', 'content', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
+    list_filter = ['title']
+    search_fields = ['title']
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
