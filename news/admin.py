875e63d6632f26c40ff9590480887dad39690b25
Kuleb1n <v_vpyt@mail.ru>
2022-11-15 23:31:51 +0300

    amendments

--- news/admin.py
+++ news/admin.py
@@ -3,10 +3,11 @@ from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content', 'created_at', 'updated_at', 'photo', 'is_published', 'category']
-    list_display_links = ['id', 'title', 'category']
+    list_display = ['id', 'title', 'category', 'content', 'created_at', 'updated_at', 'photo', 'is_published']
+    list_display_links = ['id', 'title']
     list_filter = ['title', 'created_at', 'updated_at', 'is_published', 'category']
-    search_fields = ['title', 'content', 'category']
+    search_fields = ['title', 'content']
+    list_editable = ['is_published']
 
 
 class CategoryAdmin(admin.ModelAdmin):
