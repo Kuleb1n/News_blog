c33666a80cb9744949eef0127041b81a59affc35
Kuleb1n <v_vpyt@mail.ru>
2022-11-17 00:21:39 +0300

    minor corrections

--- news/views.py
+++ news/views.py
@@ -4,23 +4,17 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     content = {
         'news': news,
-        'categories': categories,
     }
     return render(request, 'news/index.html', content)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
     content = {
         'news': news,
-        'categories': categories,
         'category': category,
     }
     return render(request, 'news/category.html', content)
-
-
