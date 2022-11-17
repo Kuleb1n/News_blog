dfea8c4f710eba0f1cc01cb91bc2d093a7622ff0
Kuleb1n <v_vpyt@mail.ru>
2022-11-17 18:48:39 +0300

    models.py (adding slug and get_absolute_url fields)

--- news/models.py
+++ news/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
     title = models.CharField('Heading', max_length=150)
+    slug = models.SlugField('URL', max_length=300, unique=True, db_index=True)
     content = models.TextField('Content')
     created_at = models.DateTimeField('Date of publication', auto_now_add=True)
     updated_at = models.DateTimeField('Publication update date', auto_now=True)
@@ -18,9 +20,13 @@ class News(models.Model):
         verbose_name_plural = 'News'
         ordering = ['-created_at']
 
+    def get_absolute_url(self):
+        return reverse('show_news', kwargs={'news_slug': self.slug})
+
 
 class Category(models.Model):
     title = models.CharField('Category', max_length=120, db_index=True)
+    slug = models.SlugField('URL', max_length=120, unique=True, db_index=True)
 
     def __str__(self):
         return self.title
@@ -29,3 +35,6 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
         ordering = ['title']
+
+    def get_absolute_url(self):
+        return reverse('show_category', kwargs={'category_slug': self.slug})
