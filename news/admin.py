47d5bb56629533e73af4996db45ce72b94a66c9a
Kuleb1n <v_vpyt@mail.ru>
2022-11-17 18:52:02 +0300

    adding a slug column and prepopulated_fields

--- news/admin.py
+++ news/admin.py
@@ -3,18 +3,20 @@ from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'content', 'created_at', 'updated_at', 'photo', 'is_published']
+    list_display = ['id', 'title', 'category', 'content', 'created_at', 'updated_at', 'photo', 'is_published', 'slug']
     list_display_links = ['id', 'title']
     list_filter = ['title', 'created_at', 'updated_at', 'is_published', 'category']
     search_fields = ['title', 'content']
     list_editable = ['is_published']
+    prepopulated_fields = {"slug": ("title",)}
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'slug']
     list_display_links = ['id', 'title']
     list_filter = ['title']
     search_fields = ['title']
+    prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(News, NewsAdmin)
