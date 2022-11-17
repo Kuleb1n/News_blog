ed8022a8830f7254f0995b5bffe82ee4f380a9c3
Kuleb1n <v_vpyt@mail.ru>
2022-11-17 18:52:53 +0300

    'category/<slug:category_slug>/' 'news/<slug:news_slug>/'

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
-    path('category/<int:category_id>/', get_category, name='get_category'),
+    path('category/<slug:category_slug>/', show_category, name='show_category'),
+    path('news/<slug:news_slug>/', show_news, name='show_news'),
 ]
