0a9a9103bb3eb1db19f939e5635dd910674c2356
Kuleb1n <v_vpyt@mail.ru>
2022-11-23 22:25:31 +0300

    adding new routes: login/ ; register/ ; logout/

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('category/<slug:category_slug>/', ShowNewsByCategory.as_view(), name='show_category'),
     path('news/<slug:news_slug>/', ShowNews.as_view(), name='show_news'),
     path('add_news/', AddNews.as_view(), name='add_news'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('logout/', logout_us, name='logout'),
 ]
