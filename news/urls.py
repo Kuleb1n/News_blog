6cf2a08bc9bd7f52c8cd5ed0aef4e7967476b799
Kuleb1n <v_vpyt@mail.ru>
2022-11-28 23:35:07 +0300

    adding routes for the user

--- news/urls.py
+++ news/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('logout/', logout_us, name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/profile/change/', ChangeUser.as_view(), name='profile_change'),
+    path('accounts/password/change/', PasswordChange.as_view(), name='password_change'),
 ]
