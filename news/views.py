b2b73b1be1ca4089e105033136cd2242f3634440
Kuleb1n <v_vpyt@mail.ru>
2022-11-29 22:00:27 +0300

    class ShowProfile

--- news/views.py
+++ news/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
@@ -17,7 +18,7 @@ class NewsIndex(ListView):
     context_object_name = 'news'
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True).select_related('category')
+        return News.objects.filter(is_published=True).select_related('category', 'user')
 
 
 class ShowNewsByCategory(ListView):
@@ -28,8 +29,8 @@ class ShowNewsByCategory(ListView):
     allow_empty = False
 
     def get_queryset(self):
-        return News.objects.filter(category__slug=self.kwargs['category_slug'], is_published=True).select_related(
-            'category')
+        return News.objects.filter(category__slug=self.kwargs['category_slug'],
+                                   is_published=True).select_related('category', 'user')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -74,7 +75,11 @@ def logout_us(request):
 
 @login_required
 def profile(request):
-    return render(request, 'news/profile.html')
+    news = News.objects.filter(user=request.user.id).select_related('category')
+    content = {
+        'news': news,
+    }
+    return render(request, 'news/profile.html', content)
 
 
 class ChangeUser(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
@@ -82,7 +87,7 @@ class ChangeUser(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     template_name = 'news/change_user.html'
     form_class = ChangeUserForm
     success_url = reverse_lazy('profile')
-    success_message = 'Данные пользователя изменены'
+    success_message = 'User data changed'
 
     def setup(self, request, *args, **kwargs):
         self.user_id = request.user.pk
@@ -100,3 +105,10 @@ class PasswordChange(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView
     template_name = 'news/password_change.html'
     success_url = reverse_lazy('profile')
     success_message = "The user's password has been changed"
+
+
+class ShowProfile(DetailView):
+    model = User
+    template_name = 'news/show_profile.html'
+    pk_url_kwarg = 'user_pk'
+    context_object_name = 'news'
