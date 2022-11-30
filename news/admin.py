e55961436419f03f6961cc32dcc9e412554f4cf0
Kuleb1n <v_vpyt@mail.ru>
2022-11-30 20:10:24 +0300

    Configuring the Administration Panel

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,41 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
+
 from .models import News, Category, User
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category', 'content', 'created_at', 'updated_at', 'photo', 'is_published', 'slug']
+    list_display = ['id', 'title', 'category', 'content', 'created_at', 'updated_at', 'get_photo', 'is_published',
+                    'slug', 'user']
     list_display_links = ['id', 'title']
     list_filter = ['title', 'created_at', 'updated_at', 'is_published', 'category']
     search_fields = ['title', 'content']
     list_editable = ['is_published']
     prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'category', 'content', 'photo', 'get_photo', 'updated_at', 'created_at', 'is_published')
+    readonly_fields = ('get_photo', 'updated_at', 'created_at')
+    actions = ['make_published', 'make_unpublished']
+
+    @admin.action(description='Mark selected news as published')
+    def make_published(self, request, queryset):
+        count = queryset.update(is_published=True)
+        self.message_user(request,
+                          'The selected news has been published.\n'
+                          f'Number of added publications: {count}')
+
+    @admin.action(description='Mark selected news unpublished')
+    def make_unpublished(self, request, queryset):
+        count = queryset.update(is_published=False)
+        self.message_user(request,
+                          'The selected news has been transferred ti the unpublished status.\n'
+                          f'Number of removed publications: {count}',
+                          messages.ERROR
+                          )
+
+    def get_photo(self, object):
+        return mark_safe(f"<img src='{object.photo.url}' width=100")
+
+    get_photo.short_description = 'Photo'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,6 +46,22 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'short_status', 'get_photo', 'date_joined',
+                    'is_staff']
+    list_display_links = ['username', 'email']
+    search_fields = ['username', 'email', 'last_name']
+    list_filter = ['date_joined']
+    fields = ('username', 'email', 'short_status', 'first_name', 'last_name', 'user_photo', 'get_photo',
+              'is_staff', 'is_active', 'is_superuser')
+    readonly_fields = ('get_photo',)
+
+    def get_photo(self, object):
+        return mark_safe(f"<img src='{object.user_photo.url}' width=100")
+
+    get_photo.short_description = "User_photo"
+
+
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
