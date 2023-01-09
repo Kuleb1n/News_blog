94a98615c9ff8e36aba636462770ae8d2f4db050
Kuleb1n <v_vpyt@mail.ru>
2023-01-09 23:39:37 +0300

    adding a route for news updates

--- news/urls.py
+++ news/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('delete/<slug:news_slug>/', DeleteNews.as_view(), name='delete'),
     path('category/<slug:category_slug>/', ShowNewsByCategory.as_view(), name='show_category'),
     path('news/<slug:news_slug>/', ShowNews.as_view(), name='show_news'),
+    path('news-update/<slug:news_slug>/', UpdateNews.as_view(), name='news-update'),
     path('add_news/', AddNews.as_view(), name='add_news'),
     path('login/', LoginUser.as_view(), name='login'),
     path('register/', RegisterUser.as_view(), name='register'),
