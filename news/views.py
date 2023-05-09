c825c5e005b6ab5917286e63b45c3ac400a5326a
Kuleb1n <v_vpyt@mail.ru>
2023-05-09 23:41:57 +0300

    adding rating

--- news/views.py
+++ news/views.py
@@ -21,6 +21,12 @@ class NewsIndex(ListView):
     def get_queryset(self):
         return News.objects.filter(is_published=True).select_related('category', 'user')
 
+    def get_context_data(self, **kwargs):
+        context = super(NewsIndex, self).get_context_data()
+        context['rating'] = Rating.objects.all().select_related()
+
+        return context
+
 
 class ShowNewsByCategory(ListView):
     paginate_by = 4
@@ -116,6 +122,10 @@ class ChangeUser(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     form_class = ChangeUserForm
     success_url = reverse_lazy('profile')
     success_message = 'User data changed'
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.user_id = None
 
     def setup(self, request, *args, **kwargs):
         self.user_id = request.user.pk
